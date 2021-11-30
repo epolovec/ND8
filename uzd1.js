dc5b9b62ad029cbd73b7f7b04cf7c62ddc0eee54
epolovec <ema.polovec@gmail.com>
2021-11-30 02:45:21 +0200

    pridejau funkcija

--- uzd1.js
+++ uzd1.js
@@ -1,9 +1,13 @@
 const skaicius1 = 5;
 const skaicius2 = 4;
 
+function Daugyba(){
 if (typeof skaicius1 === 'number' && typeof skaicius2 === 'number') {
     const daugyba = skaicius1 * skaicius2;
     console.log(daugyba);
 } else {
     console.log("Įveskite skaičius");
 }
+}
+
+Daugyba();
